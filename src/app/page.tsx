6dd499d63c46fe64f885b1339397bea2d35738c4
Jared <jareddj34@gmail.com>
2023-09-04 20:15:10 -0400

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,60 +55,6 @@ export default function Home() {
                             className="w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                         />
                     </div>
-
-                    {/* <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
-                        <div className="relative">
-                            <img
-                                src="/images/UMiamiLogo.jpg"
-                                height={128}
-                                width={128}
-                                className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-                            />
-                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                        </div>
-                    </div>
-
-                    <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36 ml-10">
-                        <div className="relative">
-                            <img
-                                src="/images/UMiamiLogo.jpg"
-                                height={128}
-                                width={128}
-                                className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-                            />
-                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                        </div>
-                        <div className="relative">
-                            <img
-                                src="/images/UMiamiLogo.jpg"
-                                height={128}
-                                width={128}
-                                className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-                            />
-                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                        </div>
-                    </div>
-
-                    <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36 ml-10 mt-10">
-                        <div className="relative">
-                            <img
-                                src="/images/UMiamiLogo.jpg"
-                                height={128}
-                                width={128}
-                                className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-                            />
-                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                        </div>
-                        <div className="relative">
-                            <img
-                                src="/images/UMiamiLogo.jpg"
-                                height={128}
-                                width={128}
-                                className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-                            />
-                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                        </div>
-                    </div> */}
                 </div>
             </div>
             <Footer />
