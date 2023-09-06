61eef9142531dd38fde7b1f673bd1b0fdcb3f2d1
Jared <jareddj34@gmail.com>
2023-09-06 00:04:50 -0400

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
 
     return (
         <div
-            className="fixed top-0 inset-x-0 h-fit border-b border-zinc-300 z-[10] py-3 bg-gray-900/5 object-cover shadow-lg"
+            className="fixed top-0 inset-x-0 h-15 border-b border-zinc-300 z-[10] py-3 bg-gray-900/5 object-cover shadow-lg"
             style={{ backgroundColor: "#234c8b" }}
         >
             <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
