d6ed632d6bd0662bf80e4b2549c8b96c48ef0c48
Javier <106848262+javiiicz@users.noreply.github.com>
2024-08-24 16:54:57 -0400

    Added Uhack to navbar, edited behavior a tad bit

---
+++ src/app/uhack/page.tsx
@@ -0,0 +1,9 @@
+import Welcome from "./components/Welcome"
+
+export default function UHack() {
+    return (
+        <div>
+            <Welcome />
+        </div>
+    )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ const Navbar: React.FC = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const mobileMenuRef = useRef(null);
     const toggleButtonRef = useRef(null);
+    
 
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -186,24 +187,32 @@ const Navbar: React.FC = () => {
                         href="/"
                         style={{ fontSize: "22px" }}
                         className="hover:text-gray-300"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            const sectionId = "#hero"; // Replace "hero" with the ID of your target section
-                            const targetSection =
-                                document.querySelector(sectionId);
 
-                            if (targetSection) {
-                                const offset = 150; // Adjust the offset value
-                                const targetPosition =
-                                    targetSection.getBoundingClientRect().top +
-                                    window.scrollY -
-                                    offset;
-                                window.scrollTo({
-                                    top: targetPosition,
-                                    behavior: "smooth",
-                                });
-                            }
-                        }}
+                        // Had to comment the smooth scroll because it was not working when adding other pages to the site
+                        // Im sure there is a way so that it scrolls when on the mian page and redirects when in the uhack page
+                        // Too lazy to figure that out now... -Javi
+
+                        // Jared why :(
+
+
+                        // onClick={(e) => {
+                        //     e.preventDefault();
+                        //     const sectionId = "#hero"; // Replace "hero" with the ID of your target section
+                        //     const targetSection =
+                        //         document.querySelector(sectionId);
+
+                        //     if (targetSection) {
+                        //         const offset = 150; // Adjust the offset value
+                        //         const targetPosition =
+                        //             targetSection.getBoundingClientRect().top +
+                        //             window.scrollY -
+                        //             offset;
+                        //         window.scrollTo({
+                        //             top: targetPosition,
+                        //             behavior: "smooth",
+                        //         });
+                        //     }
+                        // }}
                     >
                         Home
                     </Link>
@@ -211,78 +220,86 @@ const Navbar: React.FC = () => {
                         href="/#about"
                         style={{ fontSize: "22px" }}
                         className="hover:text-gray-300"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            const sectionId = "#about"; // Replace "hero" with the ID of your target section
-                            const targetSection =
-                                document.querySelector(sectionId);
+                        // onClick={(e) => {
+                        //     e.preventDefault();
+                        //     const sectionId = "#about"; // Replace "hero" with the ID of your target section
+                        //     const targetSection =
+                        //         document.querySelector(sectionId);
 
-                            if (targetSection) {
-                                const offset = 125; // Adjust the offset value
-                                const targetPosition =
-                                    targetSection.getBoundingClientRect().top +
-                                    window.scrollY -
-                                    offset;
-                                window.scrollTo({
-                                    top: targetPosition,
-                                    behavior: "smooth",
-                                });
-                            }
-                        }}
+                        //     if (targetSection) {
+                        //         const offset = 125; // Adjust the offset value
+                        //         const targetPosition =
+                        //             targetSection.getBoundingClientRect().top +
+                        //             window.scrollY -
+                        //             offset;
+                        //         window.scrollTo({
+                        //             top: targetPosition,
+                        //             behavior: "smooth",
+                        //         });
+                        //     }
+                        // }}
                     >
                         About Us
                     </Link>
 
                     <Link
-                        href="/members"
+                        href="/#members"
                         style={{ fontSize: "22px" }}
                         className="hover:text-gray-300"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            const sectionId = "#members"; // Replace "hero" with the ID of your target section
-                            const targetSection =
-                                document.querySelector(sectionId);
+                        // onClick={(e) => {
+                        //     e.preventDefault();
+                        //     const sectionId = "#members"; // Replace "hero" with the ID of your target section
+                        //     const targetSection =
+                        //         document.querySelector(sectionId);
 
-                            if (targetSection) {
-                                const offset = 100; // Adjust the offset value
-                                const targetPosition =
-                                    targetSection.getBoundingClientRect().top +
-                                    window.scrollY -
-                                    offset;
-                                window.scrollTo({
-                                    top: targetPosition,
-                                    behavior: "smooth",
-                                });
-                            }
-                        }}
+                        //     if (targetSection) {
+                        //         const offset = 100; // Adjust the offset value
+                        //         const targetPosition =
+                        //             targetSection.getBoundingClientRect().top +
+                        //             window.scrollY -
+                        //             offset;
+                        //         window.scrollTo({
+                        //             top: targetPosition,
+                        //             behavior: "smooth",
+                        //         });
+                        //     }
+                        // }}
                     >
                         Members{" "}
                     </Link>
 
                     <Link
-                        href="/rush"
+                        href="/#rush"
                         style={{ fontSize: "22px" }}
                         className="hover:text-gray-300"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            const sectionId = "#rush"; // Replace "hero" with the ID of your target section
-                            const targetSection =
-                                document.querySelector(sectionId);
+                        // onClick={(e) => {
+                        //     e.preventDefault();
+                        //     const sectionId = "#rush"; // Replace "hero" with the ID of your target section
+                        //     const targetSection =
+                        //         document.querySelector(sectionId);
 
-                            if (targetSection) {
-                                const offset = 150; // Adjust the offset value
-                                const targetPosition =
-                                    targetSection.getBoundingClientRect().top +
-                                    window.scrollY -
-                                    offset;
-                                window.scrollTo({
-                                    top: targetPosition,
-                                    behavior: "smooth",
-                                });
-                            }
-                        }}
+                        //     if (targetSection) {
+                        //         const offset = 150; // Adjust the offset value
+                        //         const targetPosition =
+                        //             targetSection.getBoundingClientRect().top +
+                        //             window.scrollY -
+                        //             offset;
+                        //         window.scrollTo({
+                        //             top: targetPosition,
+                        //             behavior: "smooth",
+                        //         });
+                        //     }
+                        // }}
                     >
                         Rush
+                    </Link>
+
+                    <Link
+                        href="/uhack"
+                        style={{ fontSize: "22px" }}
+                        className="hover:text-gray-300"
+                    >
+                        UHack
                     </Link>
                 </div>
             </div>
